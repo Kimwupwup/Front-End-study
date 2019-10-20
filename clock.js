3c9fb08aa2643b044c9a02ad7a0b44ff4c22a782
Wuppu <wndud1640@gmail.com>
2019-10-20 18:07:34 +0900

    시계 생성, 사용자 정보 저장
    setInterval(func, msec), 백틱
    classList add, remove를 통해 css조작
    form input 활용
    localStorage 활용하여 사용자 이름 저장, 불러오기

---
+++ clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+
+  clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  setInterval(getTime, 1000);
+
+  getTime();
+}
+
+init();
---
+++ gretting.js
@@ -0,0 +1,43 @@
+const greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  paintGreeting(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function init() {
+  loadName();
+}
+
+init();
