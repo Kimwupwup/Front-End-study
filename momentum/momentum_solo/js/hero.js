f7385274c7c852226400cf3061da147b6532b151
Wuppu <wndud1640@gmail.com>
2019-10-24 20:15:46 +0900

    기존의 페이지 수정
    animation효과 복습 및 css활용

---
+++ momentum/momentum_solo/js/hero.js
@@ -0,0 +1,4 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const headline = document.querySelector(".headline");
