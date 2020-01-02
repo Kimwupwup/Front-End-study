83a2845827f860d749e731e96d6dc4090027fed9
Wuppu <wndud1640@gmail.com>
2020-01-02 15:48:40 +0900

    React test

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -4,6 +4,11 @@ function App() {
 
   const [isRed, setRed] = useState(false);
   const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([
+    {name: "Wuppu", message: "Hello there."},
+    {name: "John", message: "I am John snow"},
+    {name: "Traversy", message: "I am awesome."}
+  ]);
 
   const increment = () => {
     setCount(count + 1);
@@ -15,11 +20,19 @@ function App() {
       <h1 className={isRed ? "red" : ""}>Change my color!</h1>
       <button onClick={increment}>Increment</button>
       <h1>{count}</h1>
-
-      <Tweet name="Wuppu" message="This is a random tweet"/>
-      <Tweet name="John snow" message="This is a random tweet"/>
-      <Tweet name="Winter is Comming" message="This is a random tweet"/>
-      <Tweet name="Amazon" message="?????"/>
+      <div className="sec">
+        <Tweet name="Wuppu" message="This is a random tweet"/>
+        <Tweet name="John snow" message="This is a random tweet"/>
+        <Tweet name="Winter is Comming" message="This is a random tweet"/>
+        <Tweet name="Amazon" message="?????"/>
+      </div>
+      
+      <div className="sec">
+        {users.map(user => (
+          <Tweet name={user.name} message={user.message} />
+        ))}
+      </div>
+      
     </div>
   );
 }
